b40b9cd75e99a899e1ba4ed75226e81fc3b03de9
shifa <shifayasmin23@gmail.com>
2026-05-22 10:30:58 +0530

    added new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,30 @@
-
-import  { useEffect, useState } from 'react'
-import axios from "axios"
-
+import { useEffect, useState} from 'react'
+import axios from 'axios'
 const App = () => {
-  const [value,setValue]=useState([])
-  const [type,setType]=useState("")
-  const [result, setResult]=useState("")
-
-  
-  const handle=()=>{
-    const res=value.find((n)=>n.name===type);
-      if(res){
-        setResult(res.name)
-      }else{
-        setResult("undefined")
-      }
-  }
-    useEffect(()=>{
+  const [show,setShow]=useState([])
+  const [search,setSearch]=useState("")
+  useEffect(()=>{
     axios.get("http://localhost:3000/user")
     .then(res=>{
-      setValue(res.data)
+      console.log(res)
+      setShow(res.data)
     })
     .catch(err=>{
       console.log(err)
     })
-    },[])
-  
+  },[])
+
+   const filterData=show.filter((item)=>
+    item.name.toLowerCase().includes(search.toLowerCase())
+   )
+
   return (
- 
-    <>
-    <ul>
-       {value.map((n,index)=>(
-      <li key={index}>{n.name}</li>
-     ))}
-    </ul>
-    <input value={type} placeholder='select value' onChange={(e)=>setType(e.target.value)}/>
-    <button onClick={handle}>click</button>
-    <p>{result}</p>
-    </>
+    <div>
+      <input value={search} placeholder="enter value" onChange={(e)=>setSearch(e.target.value)}/>
+      {filterData.map((n,index)=>(
+        <h1 key={index}>{n.name}</h1>
+      ))}
+    </div>
   )
 }
 
